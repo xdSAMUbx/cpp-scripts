5054e3e95a8354eafcde0b668d83335a0768ae6c
Samuel Calderon <samuelcalderon1202@hotmail.com>
2025-08-23 18:17:53 -0500

    bubble sort

---
+++ include/sortAlgorithms.h
@@ -0,0 +1,10 @@
+#ifndef sortAlgorithms_h
+#define sortAlgorithms_h
+
+#include <vector>
+
+void showVector(const std::vector<int> &vec);
+std::vector<int> linspace(const int &ini, const int &fin, const int &step);
+std::vector<int> randVector(const int &n, const int &x);
+
+#endif
---
+++ src/Algorithms/sortAlgorithms.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> bubbleSort(std::vector<int> &vec)
+{
+    int temp;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        for (int j = 0; j < vec.size() + i - 1; j++)
+        {
+            if (vec[j] > vec[j + 1])
+            {
+                temp = vec[j];
+                vec[j] = vec[j + 1];
+                vec[j + 1] = temp;
+            }
+        }
+    }
+    return vec;
+}
