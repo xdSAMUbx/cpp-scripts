d02504cf199bf2fa5a9b22fe20a8cf746d7a13e0
Samuel Calderon <samuelcalderon1202@hotmail.com>
2025-09-05 23:52:08 -0500

    grilla 1

--- src/DSA/Matrix/matrix1.cpp
+++ src/DSA/Matrix/matrix1.cpp
@@ -22,6 +22,29 @@ std::array<int,2> findMat(const int& n)
   return {res, res2};
 }
 
+template<typename T>
+std::vector<Point<T>> grid(const T& min, const T& max, const int& n)
+{
+  std::vector<Point<T>> res;
+  std::array<int,2> shape;
+  shape = findMat(n);
+  int row = shape.at(0);
+  int col = shape.at(1);
+  res.resize(row*col);
+  T stpX = (max - min) / static_cast<T>(row - 1);
+  T stpY = (max - min) / static_cast<T>(col - 1);
+  for (int i = 0; i < row; i++)
+  {
+    T x = min + static_cast<T>(i) * stpX;
+    for (int j = 0; j < col; j++)
+    {
+      T y = min + static_cast<T>(j) * stpY;
+      res[i * col + j] = Point<T>{x, y};
+    }
+  } 
+  return res;
+}
+
 int main()
 {
   int n; double max;
@@ -32,6 +55,10 @@ int main()
 
   std::array<int,2> shape = findMat(n);
   std::cout << shape[0] << ", " << shape[1] << "\n";
+  
+  std::vector<Point<double>> grid1 = grid(1.0,max,n);
+  for (int i = 0; i < n; i++)
+    std::cout << "(" << grid1[i].x << ", " << grid1[i].y << ")\n";
 
   return 0;
 }
