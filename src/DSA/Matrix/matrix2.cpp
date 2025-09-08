f299d933d6955002ab69e508bffd3e817859d623
Samuel Calderon <samuelcalderon1202@hotmail.com>
2025-09-07 21:34:15 -0500

    añadiendo data

---
+++ src/DSA/Matrix/matrix2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+template <typename T> struct Point {
+  T x, y;
+};
+
+template <typename T> std::vector<Point<T>> grid(const T *max, const int *n) {
+  std::vector<Point<T>> res;
+  return res;
+}
+
+int main() {
+  std::vector<Point<double>> points;
+  return 0;
+}
